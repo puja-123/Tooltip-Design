2a4649d07bbbaad71cdbfe3ed93c7ee03e5be870
puja-123 <bhard.puja@gmail.com>
2020-08-12 21:50:34 +0530

    Updates

---
+++ precache-manifest.748d497cae294669dd194e7ecdef2035.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebcdb63c74a1c5c3024a0e9312810941",
+    "url": "/Tooltip-Design/index.html"
+  },
+  {
+    "revision": "bcc63c44842bae25201f",
+    "url": "/Tooltip-Design/static/css/main.ba1198f5.chunk.css"
+  },
+  {
+    "revision": "e60a30c4221839c5ce06",
+    "url": "/Tooltip-Design/static/js/2.47a982d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tooltip-Design/static/js/2.47a982d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcc63c44842bae25201f",
+    "url": "/Tooltip-Design/static/js/main.91cb393c.chunk.js"
+  },
+  {
+    "revision": "ac2fb859223705db1b6b",
+    "url": "/Tooltip-Design/static/js/runtime-main.d6f232b0.js"
+  }
+]);
